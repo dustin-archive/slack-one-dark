a16a63369bfeaf6484a3e45f0af42bc53065203d
Dustin Dowell <dustindowell22@gmail.com>
2016-12-14 12:24:20 -0600

    remove sss

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 const gulp = require('gulp')
   const sass = require('gulp-sass')
   const postcss = require('gulp-postcss')
-    const sugarss = require('sugarss')
     const autoprefixer = require('autoprefixer')
   const rename = require('gulp-rename')
 
-gulp.task('sss', () => {
-  return gulp.src('slack.sss')
-    .pipe(postcss([], { parser: sugarss }))
-    .pipe(rename({ extname: '.scss' }))
-    .pipe(gulp.dest(''))
-})
-
-gulp.task('scss', ['sss'], () => {
+gulp.task('scss', () => {
   return gulp.src('slack.scss')
     .pipe(sass({ style: 'expanded' }).on('error', sass.logError))
     .pipe(postcss([ autoprefixer ]))
